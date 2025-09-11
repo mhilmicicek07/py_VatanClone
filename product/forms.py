2709b1b89f67712f153d92864a2e9b636a42c588
mhilmicicek07 <m.hilmicicek_07@icloud.com>
2025-09-11 20:04:52 +0200

    update

--- product/forms.py
+++ product/forms.py
@@ -1,22 +1,40 @@
 from django import forms
-from .models import *
+from .models import Urun
+
 
 class UrunEkle(forms.ModelForm):
     class Meta:
         model = Urun
-        fields = ('urun_name','urun_description','urun_price','urun_image','urun_category')
-        label = {
-            'urun_name':'Ürün Adı',
-            'urun_description':'Ürün Açıklaması',
-            'urun_price':'Ürün Fiyatı',
-            'urun_category':'Ürün Kategorisi',
+        fields = ["urun_name", "urun_description", "urun_price", "urun_image", "urun_category"]
+        widgets = {
+            "urun_description": forms.Textarea(attrs={"rows": 3}),
         }
 
-        widgets = {
-            'urun_name': forms.TextInput(attrs={'class':'form-control'}),
-            'urun_description': forms.TextInput(attrs={'class':'form-control'}),
-            'urun_price': forms.NumberInput(attrs={'class':'form-control'}),
-            # 'urun_image': forms.FileInput(attrs={'class':'form-control'}),
-            # 'urun_image': forms.CharField(is_hidden=False),
-            'urun_category': forms.Select(attrs={'class':'form-select'})
-        }
+    def clean_urun_price(self):
+        price = self.cleaned_data.get("urun_price")
+        if price is not None and price <= 0:
+            raise forms.ValidationError("Fiyat 0'dan büyük olmalı!")
+        return price
+
+# from django import forms
+# from .models import *
+
+# class UrunEkle(forms.ModelForm):
+#     class Meta:
+#         model = Urun
+#         fields = ('urun_name','urun_description','urun_price','urun_image','urun_category')
+#         label = {
+#             'urun_name':'Ürün Adı',
+#             'urun_description':'Ürün Açıklaması',
+#             'urun_price':'Ürün Fiyatı',
+#             'urun_category':'Ürün Kategorisi',
+#         }
+
+#         widgets = {
+#             'urun_name': forms.TextInput(attrs={'class':'form-control'}),
+#             'urun_description': forms.TextInput(attrs={'class':'form-control'}),
+#             'urun_price': forms.NumberInput(attrs={'class':'form-control'}),
+#             # 'urun_image': forms.FileInput(attrs={'class':'form-control'}),
+#             # 'urun_image': forms.CharField(is_hidden=False),
+#             'urun_category': forms.Select(attrs={'class':'form-select'})
+#         }
--- product/models.py
+++ product/models.py
@@ -1,36 +1,71 @@
 from django.db import models
-from django.utils.text import slugify
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
-# Create your models here.
+
 class Kategori(models.Model):
-    kategori_name = models.CharField(max_length=50)
-    # kategori_image = models.FileField(upload_to='kategori_images')
-    # kategori_slug = models.SlugField(unique=True, blank=True)
-
-    # def save(self, *args, **kwargs):
-    #     self.kategori_slug = slugify(self.kategori_name)
-    #     super().save(*args, **kwargs)
+    kategori_name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.kategori_name
 
+
 class Urun(models.Model):
-
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-
-    urun_name = models.CharField(max_length=100)
-    urun_description = models.CharField(max_length=250)
+    urun_name = models.CharField(max_length=200)
+    urun_description = models.TextField(max_length=250)
     urun_price = models.DecimalField(max_digits=10, decimal_places=2)
-    urun_image = models.CharField(max_length=500)
-
+    urun_image = models.ImageField(upload_to="uploads/", null=True, blank=True)
     urun_category = models.ForeignKey(Kategori, on_delete=models.CASCADE, null=True)
-
     urun_slug = models.SlugField(unique=True, blank=True)
 
     def save(self, *args, **kwargs):
-        self.urun_slug = slugify(self.urun_name)
+        if not self.urun_slug:
+            base_slug = slugify(self.urun_name)
+            slug = base_slug or "urun"
+            counter = 1
+            while Urun.objects.filter(urun_slug=slug).exclude(pk=self.pk).exists():
+                counter += 1
+                slug = f"{base_slug}-{counter}"
+            self.urun_slug = slug
         super().save(*args, **kwargs)
 
     def __str__(self):
         return self.urun_name
+
+# from django.db import models
+# from django.utils.text import slugify
+# from django.contrib.auth.models import User
+
+# # Create your models here.
+# class Kategori(models.Model):
+#     kategori_name = models.CharField(max_length=50)
+#     # kategori_image = models.FileField(upload_to='kategori_images')
+#     # kategori_slug = models.SlugField(unique=True, blank=True)
+
+#     # def save(self, *args, **kwargs):
+#     #     self.kategori_slug = slugify(self.kategori_name)
+#     #     super().save(*args, **kwargs)
+
+#     def __str__(self):
+#         return self.kategori_name
+
+# class Urun(models.Model):
+
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+#     urun_name = models.CharField(max_length=100)
+#     urun_description = models.CharField(max_length=250)
+#     urun_price = models.DecimalField(max_digits=10, decimal_places=2)
+#     urun_image = models.CharField(max_length=500)
+
+#     urun_category = models.ForeignKey(Kategori, on_delete=models.CASCADE, null=True)
+
+#     urun_slug = models.SlugField(unique=True, blank=True)
+
+#     def save(self, *args, **kwargs):
+#         self.urun_slug = slugify(self.urun_name)
+#         super().save(*args, **kwargs)
+
+#     def __str__(self):
+#         return self.urun_name
